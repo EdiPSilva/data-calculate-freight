16c48965ef44843c2f985bfd6bdf417c03957be1
edisilva <edipaulinodasilva@gmail.com>
2023-03-09 09:48:50 -0300

    iniciando configuração do projeto

---
+++ src/main/java/br/com/java/datacalculatefreight/DataCalculateFreightApplication.java
@@ -0,0 +1,13 @@
+package br.com.java.datacalculatefreight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCalculateFreightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataCalculateFreightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/java/datacalculatefreight/DataCalculateFreightApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.java.datacalculatefreight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCalculateFreightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
