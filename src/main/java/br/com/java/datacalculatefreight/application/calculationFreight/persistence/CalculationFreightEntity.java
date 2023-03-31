fff9eecfa3b7d65b743ad247c02b88979cf2c873
edisilva <edipaulinodasilva@gmail.com>
2023-03-31 08:58:08 -0300

    realizado mapeamento da entity e iniciando o mapeamento do response

--- src/main/java/br/com/java/datacalculatefreight/application/calculationFreight/persistence/CalculationFreightEntity.java
+++ src/main/java/br/com/java/datacalculatefreight/application/calculationFreight/persistence/CalculationFreightEntity.java
@@ -1,11 +1,13 @@
 package br.com.java.datacalculatefreight.application.calculationFreight.persistence;
 
+import br.com.java.datacalculatefreight.application.shippingCompany.persistence.ShippingCompanyEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Data
 @Builder
@@ -19,4 +21,38 @@ public class CalculationFreightEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(columnDefinition = "serial")
     private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "shipping_company_id")
+    private ShippingCompanyEntity shippingCompanyEntity;
+
+    @Column(name = "sender_postal_code")
+    private String senderPostalCode;
+
+    @Column(name = "destiny_postal_code")
+    private String destinyPostalCode;
+
+    @Column(name = "width")
+    private Long width;
+
+    @Column(name = "height")
+    private Long height;
+
+    @Column(name = "length")
+    private Long length;
+
+    @Column(name = "cubage")
+    private Long cubage;
+
+    @Column(name = "weight")
+    private Double weight;
+
+    @Column(name = "freight_value")
+    private Double freightValue;
+
+    @Column(name = "date_create")
+    private LocalDateTime dateCreate;
+
+    @Column(name = "date_update")
+    private LocalDateTime dateUpdate;
 }
--- src/main/java/br/com/java/datacalculatefreight/application/calculationFreight/resources/CalculationFreightResponse.java
+++ src/main/java/br/com/java/datacalculatefreight/application/calculationFreight/resources/CalculationFreightResponse.java
@@ -1,9 +1,54 @@
 package br.com.java.datacalculatefreight.application.calculationFreight.resources;
 
+import br.com.java.datacalculatefreight.application.shippingCompany.resources.ShippingCompanyResponse;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CalculationFreightResponse {
+
+    //TODO - utilizar do response da transportadora no construtor desta classe
+
+    @ApiModelProperty(notes = "Id do cálculo de frete", example = "1", required = true)
+    private Long id;
+
+    @ApiModelProperty(notes = "Transportadora do frete", example = "1", required = false)
+    private ShippingCompanyResponse shippingCompany;
+
+    @ApiModelProperty(notes = "Remetente cep", example = "1", required = true)
+    private String senderPostalCode;
+
+    @ApiModelProperty(notes = "Destino cep", example = "1", required = true)
+    private String destinyPostalCode;
+
+    @ApiModelProperty(notes = "Largura", example = "1", required = true)
+    private Long width;
+
+    @ApiModelProperty(notes = "Altura", example = "1", required = true)
+    private Long height;
+
+    @ApiModelProperty(notes = "Comprimento", example = "1", required = true)
+    private Long length;
+
+    @ApiModelProperty(notes = "Cubagem", example = "1", required = true)
+    private Long cubage;
+
+    @ApiModelProperty(notes = "Peso", example = "1", required = true)
+    private Double weight;
+
+    @ApiModelProperty(notes = "Valor do frete", example = "1", required = true)
+    private Double freightValue;
+
+    @ApiModelProperty(notes = "Data de cadastro", example = "dd/MM/yyyy hh:mm:ss", required = true)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy hh:mm:ss")
+    private LocalDateTime dateCreate;
+
+    @ApiModelProperty(notes = "Data de atualização", example = "dd/MM/yyyy hh:mm:ss", required = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy hh:mm:ss")
+    private LocalDateTime dateUpdate;
 }
